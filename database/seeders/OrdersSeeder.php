be405f450a26a4fe8c8749b2d2824d3f6c650366
zkemoche <Zkemoche@gmail.com>
2024-10-24 12:46:44 +0300

    Email verification

--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OrdersSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class OrdersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $orders = [
+            
+        
+            'user_id' => '1',
+            'order_type' => 'food',
+            'order_status' => 'Not Ready',
+            'order_total' => '800'
+        ];
+
+    DB::table('orders')->insert($orders);
     }
 }

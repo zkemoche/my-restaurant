0e7d7688d6f14b9b3be4baf37cd7d3d6c1fd1c4a
zkemoche <Zkemoche@gmail.com>
2024-10-30 17:42:52 +0300

    fine tuning

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,46 +11,57 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-            'user_type' => 'required|string|max:255',
-            'phone' => 'required|string|max:15',
-            'location' => 'required|string|max:255',
-        ]);
+                $request->validate([
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|max:255|unique:users',
+                    'password' => 'required|string|min:8',
+                    'phone' => 'required|string|max:255',
+                    'location' => 'required|string|max:255',
+                    'user_type' => 'required|string|max:255',
+                ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'user_type' => $request->name,
-            'phone' => $request->name,
-            'location' => $request->name,
-        ]);
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                    'phone' => $request->phone,
+                    'location' => $request->location,
+                    'user_type' => $request->user_type,
+                    
+                ]);
 
-        return response()->json([
-            'user' => $user,
-            'token' => $user->createToken('token-name')->plainTextToken,
-        ], 201);
+                // $data = $request->all();
+                // $data['password'] = Hash::make($data['password']);
+                // $user = User::create($data);
+
+                return response()->json([
+                    'user' => $user,
+                    'token' => $user->createToken('token-name')->plainTextToken,
+                ], 201);
     }
     public function login(Request $request)
     {
         $request->validate([
             'email' => 'required|string|email',
-            'password' => 'required|string',            
+            'password' => 'required|string',
         ]);
 
-        $user = User::where('email',$request->email)->first();
+        $user = User::where('email', $request->email)->first();
 
-        if (! $user || ! Hash::check($request->password, $user->password)){
-            throw ValidationException::withMessage([
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
+
         return response()->json([
-            'user' =>$user,
+            'user' => $user,
             'token' => $user->createToken('token-name')->plainTextToken,
         ]);
+    }
+
+    public function logout(){
+        $user = Auth::user();
+        return $user;
     }
 }
